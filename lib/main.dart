eecff169986962e52946e7e65ba664c10b4a63dc
Smith Huamani Hilario <smithhuamanihilario@Smiths-MacBook-Pro.local>
2020-08-07 18:09:14 -0500

    Remove all

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'package:covid19/src/pages/country_list_page.dart';
-import 'package:covid19/src/pages/infected_list_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,6 +7,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Covid-19',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(accentColor: Colors.black),
       title: 'Covid-19',
       initialRoute: '/',
@@ -17,15 +31,4 @@ class MyApp extends StatelessWidget {
         '/': (BuildContext context) => CountryListPage(),
         'infected': (BuildContext context) => InfectedListPage(),
       },
-    );
-    /*CupertinoApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Covid-19',
-      initialRoute: '/',
-      routes: {
-        '/': (BuildContext context) => CountryListPage(),
-        'infected': (BuildContext context) => InfectedListPage(),
-      },
     );*/
-  }
-}
