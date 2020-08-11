a5be2b2db3577b8e655d7977baebc1369497758e
Smith Huamani Hilario <smithhuamanihilario@Smiths-MacBook-Pro.local>
2020-08-11 10:32:56 -0500

    commit in country

--- lib/src/models/country.dart
+++ lib/src/models/country.dart
@@ -3,12 +3,17 @@ class Country {
 
   Country();
 
-  Country.fromJson(List<dynamic> json) {
+  /*Country.fromJson2(List<dynamic> json) {
     if (json == null) return;
-
     for (var i in json) {
       final country = i.toString();
       items.add(country);
     }
+  }*/
+
+  Country.fromJson(Map<String, dynamic> json) {
+    final response = json['response'];
+    if (response == null) return;
+    items = List<String>.from(response);
   }
 }
